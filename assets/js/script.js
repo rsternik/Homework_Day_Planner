fb1edd06ce64c47e784f81dd60de113079779394
rsternik <rsternik.wg@gmail.com>
2021-10-16 01:19:57 -0700

    Rows now data-states assigned and will now be labled with the correct style color for past present and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 // Variables
 let container = $('.container')
 let currentDay = $('#currentDay')
+plannerRow = $('<div>')
 // Moment Setup
 let now = moment()
-let hours = [];
+let timeFormat = now.format('h A')
+let hours = []
+let hourState = []
+let dayState = []
 let time = now.format("dddd, MMMM Do YYYY, h:mm:ss a")
 
 //Function
@@ -11,15 +15,20 @@ let time = now.format("dddd, MMMM Do YYYY, h:mm:ss a")
 function dayScheduler() {
     // Loop
     // 
-    for (let hour = 9; hour < 18; hour++) {
-        hours.push(moment({ hour }).format('hh:mm A'));
+    for (let hour = 00; hour < 24; hour++) {
+        hours.push(moment({ hour }).format('hh:mm A'))
+        hourState.push(moment({ hour }).format('H A'))   
     }
     // Loop 
     // Element creation and display
     for (var i = 0; i <= 8; i++) {
         // Elements & Attributes
-        let plannerRow = $('<div>')
+        // Rows
+        plannerRow = $('<div>')
         plannerRow.attr('class', 'row')
+        plannerRow.attr('data-state', hourState[i])
+        let rowState = plannerRow.attr('data-state')
+        
         // Hour
         let hourEl = $('<div>')
         hourEl.attr('class', 'hour')
@@ -27,13 +36,26 @@ function dayScheduler() {
         // Text box
         let description = $('<textArea>')
         description.attr('class', 'description')
-        description.text('Placeholder')
         // Save Button
         let saveBtn = $('<button>')
         saveBtn.attr('class', 'saveBtn')
         saveBtn.text('Save')
         // Element Output
         container.append(plannerRow.append(hourEl).append(description).append(saveBtn))
+        //Styling rows with past, present, & future classes
+        if (rowState > now.format('H A')){
+            description.attr('class', 'future')
+            console.log('future')
+        }
+        else if (rowState < now.format('H A')){
+            description.attr('class', 'past')
+               console.log('past')
+        }
+        else {
+            description.attr('class', 'present')
+               console.log('present')
+        }
+
     }
     // Click Event
     document.addEventListener('click', function () {
