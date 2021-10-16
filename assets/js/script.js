fc758208d9e1198d1252e956eef562a87e3e4f28
rsternik <rsternik.wg@gmail.com>
2021-10-15 21:11:20 -0700

    Current Time, Rows and Elements now display on page with proper hour assigned to ech row. Panner does not save currently and also does not  display past, present, future colors for each row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,49 @@
-
+// Variables
 let container = $('.container')
-
 let currentDay = $('#currentDay')
-
+// Moment Setup
 let now = moment()
-time = now.format("dddd, MMMM Do YYYY, h:mm:ss a")
-currentDay.append(time)
+let hours = [];
+let time = now.format("dddd, MMMM Do YYYY, h:mm:ss a")
 
-
-
-
-for (var i = 0; i <= 8; i++) {
-    console.log('LOOPING')
-
-    
- 
-    let plannerRow = $('<div>')
-    plannerRow.attr('class', 'row')
-
-    let hour = $('<div>')
-    hour.attr('class', 'hour')
-    hour.text('12PM')
-
-    let description = $('<textArea>')
-    description.attr('class', 'description')
-    description.text('Placeholder')
-
-    let saveBtn = $('<button>')
-    saveBtn.attr('class', 'saveBtn')
-    saveBtn.text('Save')
-
-    container.append(plannerRow.append(hour).append(description).append(saveBtn))
+//Function
+//Day Scheduler
+function dayScheduler() {
+    // Loop
+    // 
+    for (let hour = 9; hour < 18; hour++) {
+        hours.push(moment({ hour }).format('hh:mm A'));
+    }
+    // Loop 
+    // Element creation and display
+    for (var i = 0; i <= 8; i++) {
+        // Elements & Attributes
+        let plannerRow = $('<div>')
+        plannerRow.attr('class', 'row')
+        // Hour
+        let hourEl = $('<div>')
+        hourEl.attr('class', 'hour')
+        hourEl.text(hours[i])
+        // Text box
+        let description = $('<textArea>')
+        description.attr('class', 'description')
+        description.text('Placeholder')
+        // Save Button
+        let saveBtn = $('<button>')
+        saveBtn.attr('class', 'saveBtn')
+        saveBtn.text('Save')
+        // Element Output
+        container.append(plannerRow.append(hourEl).append(description).append(saveBtn))
+    }
+    // Click Event
+    document.addEventListener('click', function () {
+        console.log('hello')
+    })
 }
-
-
-
+// Function
+// Initialize
+function init() {
+    currentDay.append(time)
+    dayScheduler()
+}
+init()
